e2e3060b95fff2de5fbabd611b672fa8185c8107
gaurravlokhande <gaurravlokhande@gmail.com>
2023-09-20 16:27:09 +0530

    After Meet Changes

--- force-app/main/default/lwc/oppSearchOne/oppSearchOne.js
+++ force-app/main/default/lwc/oppSearchOne/oppSearchOne.js
@@ -1,18 +1,19 @@
+// oppSearchOne.js
 import { LightningElement, track, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import ShowOpportunityFields from '@salesforce/apex/OpportunityDataController.ShowOpportunityFields';
 
-
 const columns = [
-    { label: 'Opportunity Name', fieldName: 'Name' },
+    { label: 'Opportunity Name', fieldName: 'Id', type: 'button', typeAttributes: { label: { fieldName: 'Name' }, name: 'viewOpportunity' } },
     { label: 'Opportunity Description', fieldName: 'Description' },
     { label: 'Close Date', fieldName: 'CloseDate', type: 'date' },
-    { label: 'Account Name', fieldName: 'Account_Name__c' },
+    { label: 'Account Name', fieldName: 'AccountName' },
     { label: 'Recent Contact Name', fieldName: 'Recent_Contact_Name__c' },
     { label: 'Recent Contact Email', fieldName: 'Recent_Contact_Email__c' },
     { label: 'Recent Contact Number', fieldName: 'Recent_Contact_No__c' }
 ];
 
-export default class OppSearchOne extends LightningElement {
+export default class OppSearchOne extends NavigationMixin(LightningElement) {
     columns = columns;
     @track storetabledata;
     @track filteredData = [];
@@ -21,8 +22,11 @@ export default class OppSearchOne extends LightningElement {
 
     @wire(ShowOpportunityFields)
     wiredResult(result) {
-        if (result) {
-            this.storetabledata = result;
+        if (result.data) {
+            this.storetabledata = result.data.map(oppdata => ({
+                ...oppdata,
+                AccountName: oppdata.Account.Name
+            }));
             this.filterData();
             this.error = undefined;
         } else if (result.error) {
@@ -38,16 +42,35 @@ export default class OppSearchOne extends LightningElement {
     }
 
     filterData() {
-        if (!this.storetabledata || !this.storetabledata.data) {
+        if (!this.storetabledata || this.storetabledata.length === 0) {
             return;
         }
 
-        this.filteredData = this.storetabledata.data.filter(item => {
+        this.filteredData = this.storetabledata.filter(item => {
             return (
                 item.Name.toLowerCase().includes(this.searchTerm) ||
-                item.Account_Name__c.toLowerCase().includes(this.searchTerm) ||
+                item.AccountName.toLowerCase().includes(this.searchTerm) ||
                 item.Recent_Contact_Name__c.toLowerCase().includes(this.searchTerm)
             );
+        });
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        if (actionName === 'viewOpportunity') {
+            const row = event.detail.row;
+            this.navigateToOpportunity(row.Id);
+        }
+    }
+
+    navigateToOpportunity(recordId) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: recordId,
+                objectApiName: 'Opportunity',
+                actionName: 'view',
+            },
         });
     }
 }
